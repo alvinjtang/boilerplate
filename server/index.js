108a706028840f3807160c2444e2b69a82b2a54f
alvinjtang <alvintangdesigns@gmail.com>
2020-02-01 00:49:05 -0500

    make it better

--- server/index.js
+++ server/index.js
@@ -21,8 +21,9 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).send(err.message || 'Internal server error.');
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+var server = app.listen(process.env.PORT || 3000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
 });
 
 module.exports = app;
